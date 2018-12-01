caeadfa84c1a2c4b6cc695deb2dd3fb1beddc356
cronatus <mwilliamsgamesdev@gmail.com>
2018-12-01 04:03:14 +0000

    implemented basic controllers
    implemented basic player and AI controllers, as well as a placeholder enemy sprite

---
+++ LDReptarGames/Assets/Scripts/BasicEnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyController : MonoBehaviour {
+
+    public float speed;
+    private Transform playerPos;
+
+    void Start() {
+
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    void Update() {
+
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+
+    }
+
+}
