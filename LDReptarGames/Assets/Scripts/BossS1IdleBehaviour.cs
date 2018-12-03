00ab5022deae146a82f7551d8b0cee07f2f1a689
cronatus <mwilliamsgamesdev@gmail.com>
2018-12-03 04:06:36 +0000

    Added in Giant Slime Boss and set him up somewhat

---
+++ LDReptarGames/Assets/Scripts/BossS1IdleBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossS1IdleBehaviour : StateMachineBehaviour {
+
+    public float timer;
+    public float minTime;
+    public float maxTime;
+
+    // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        timer = Random.Range(minTime, maxTime);
+
+	}
+
+	// OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	
+        if (timer <= 0) {
+
+            animator.SetTrigger("attack");
+
+        } else {
+
+            timer -= Time.deltaTime;
+
+        }
+
+	}
+
+	// OnStateExit is called before OnStateExit is called on any state inside this state machine
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	
+
+
+	}
+
+
+}
--- LDReptarGames/Assets/Scripts/TutBossController.cs
+++ LDReptarGames/Assets/Scripts/TutBossController.cs
@@ -5,26 +5,44 @@ using UnityEngine.UI;
 
 public class TutBossController : MonoBehaviour {
 
-    public int bossHealth; // The Boss' health
+    public int maxBossHealth; // the Boss' max health
+    private int bossHealth; // The Boss' health
     public int damage; // The Damage the Boss does
-    private float timeBtwDamage = 1.0f;
+    private float timeBtwDamage = 1.5f;
 
-
+    public Slider healthBar;
+    private Animator anim;
     public bool isDead;
+    
 
 	// Use this for initialization
 	void Start () {
-		
+
+        anim = GetComponent<Animator>();
+        bossHealth = maxBossHealth;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (bossHealth <= maxBossHealth/2) {
+
+            anim.SetTrigger("stageTwo");
+
+        } else if (bossHealth <= 0) {
+
+            anim.SetTrigger("death");
+
+        }
 		
         if(timeBtwDamage > 0) {
 
             timeBtwDamage -= Time.deltaTime;
 
         }
+
+        healthBar.value = bossHealth;
 
     }
 
