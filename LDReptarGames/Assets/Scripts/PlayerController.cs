3cced391f879578c550cde35ef9862b515b97b76
AlanW95 <alan.williams.1995@gmail.com>
2018-12-03 23:30:44 +0000

    Player Health Bar Added and Depletes
    Player Health bar has been added

--- LDReptarGames/Assets/Scripts/PlayerController.cs
+++ LDReptarGames/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public int playerHealth; //Player's health
+    public int numberofHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
     public float speed; // The base speed of the player
 
@@ -25,6 +31,25 @@ public class PlayerController : MonoBehaviour {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); // Detect directional input from either WASD or the ARROW KEYS to determine the intended movemenet of the player
         moveVelocity = moveInput.normalized * speed; // Move the player according to the input above
         Debug.Log(moveVelocity);
+
+        if (playerHealth > numberofHearts) {
+            playerHealth = numberofHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < playerHealth) {
+                hearts[i].sprite = fullHeart;
+            } else {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numberofHearts) {
+                hearts[i].enabled = true;
+            } else {
+                hearts[i].enabled = false;
+            }
+        }
         
         if (playerHealth <= 0) {
             //load game over scene
